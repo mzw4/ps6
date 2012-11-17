b7bf53b232ea633c78162d702a39c22db9da7922
Michael Wang <mzw4@cornell.edu>
2012-11-17 16:17:36 -0500

    update game
    init_game

--- game/game.ml
+++ game/game.ml
@@ -11,13 +11,13 @@ type phase =
 
 type game = State.state
 
+let game = State.create ()
 
 let game_datafication (g:game) : game_status_data =
-	failwith "not implemented"
-	
+  match g with {game_data = data} -> data	
 	
 let game_from_data (game_data:game_status_data) : game  = 
-	failwith "not implemented"
+  game.game_data <- game_data
 
 let handle_step (g:game) (ra:command) (ba:command) : game_output =
 	let (phase, state) = g in
@@ -40,4 +40,23 @@ let handle_step (g:game) (ra:command) (ba:command) : game_output =
 	failwith ""
 
 let init_game () =
-	failwith "implement me!"
+  let alines = read_lines "./game/attack.txt" in
+  let attacks = 
+    List.fold_left (fun acc line -> 
+      match Str.split (Str.regexp_string " ") line with
+      | [n; e; pp; pow; acc; crit; eff; eff_chance] ->
+        {name = name; element = e; max_pp = pp; pp_remaining = pp; power = pow;
+        accuracy = acc; crit_chance = crit; effect = eff} :: acc
+      | _ -> failwith "incorrect attack input format"
+    ) [] alines in
+  let slines = read_lines "./game/attack.txt" in
+  let steammon = 
+    List.fold_left (fun acc line -> 
+      match Str.split (Str.regexp_string " ") line with
+      | [n; e; pp; pow; acc; crit; eff; eff_chance] ->
+        {name = name; element = e; max_pp = pp; pp_remaining = pp; power = pow;
+        accuracy = acc; crit_chance = crit; effect = eff} :: acc
+      | _ -> failwith "incorrect attack input format"
+    ) [] slines in
+
+  
