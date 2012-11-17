4162eec6db32970b6e5210b003c9f57655b686b0
Matt Oey <mso32@cornell.edu>
2012-11-17 02:10:58 -0500

    Update game

--- game/game.ml
+++ game/game.ml
@@ -2,7 +2,6 @@ open Definitions
 open Util
 open Constants
 open Netgraphics
-open State
 
 type phase =
   | Init
@@ -12,27 +11,27 @@ type phase =
 
 type game = phase * State.state
 
-let game_datafication g =
-	failwith "not implemented"
+
+let game_datafication (g:game) : game_status_data =
+	(phase, state) = g in
 	
-let game_from_data game_data = 
+	
+	
+let game_from_data (game_data:game_status_data) : game  = 
 	failwith "not implemented"
 
-let handle_step (g:game) (ra: command) (ba: command) : game_output =
-	(phase, state) = g
-	match phase with
-	| Init -> begin
-		
-		end
-	| Draft -> begin
-		
-		end
-	| Inventory -> begin
-		
-		end
-	| Battle -> begin
-		
-		end
+let handle_step (g:game) (ra:command) (ba:command) : game_output =
+	let (phase, state) = g in
+	let helper (c:command) (s:state) : state =
+		match c with
+		| Action act -> 
+			match phase with
+			| Draft ->
+				match act with
+				| 
+		| _ -> failwith ""
+	in	 
+	failwith ""
 
 let init_game () =
 	failwith "implement me!"
