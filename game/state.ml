0d64843682a81161275b8bc51851187a260ef883
Matt Oey <mso32@cornell.edu>
2012-11-17 16:18:32 -0500

    Update state

--- game/state.ml
+++ game/state.ml
@@ -1,9 +1,8 @@
 type state = {
-	current_phase: phase;
-	current_red_steammon: steammon;
-	current_blue_steammon: steammon;
-	game_data: game_status_data;
-	
+	mutable current_phase: phase;
+	mutable current_red_steammon: steammon option;
+	mutable current_blue_steammon: steammon option;
+	mutable game_data: game_status_data option;
 	}
 	
 type phase = 
@@ -11,3 +10,30 @@ type phase =
 	| SelectSteammon
 	| SelectItems
 	| Battle
+
+let create : state = 
+	{current_phase = Init;
+	current_red_steammon = None;
+	current_blue_steammon = None;
+	game_data = None;
+	}
+
+let set_phase (st: state) (ph: phase) : unit =
+	st.phase <- ph;
+
+let set_red_starter (st: state) (steam: steammon) : unit =
+	st.current_red_steammon <- steam;
+	
+let set_blue_starter (st: state) (steam: steammon) : unit =
+	st.current_blue_steammon <- steam;
+
+let set_game_data (st: state) (data: game_status_data) : unit =
+	st.game_data <- data;
+
+
+	
+
+	
+
+	
+	
