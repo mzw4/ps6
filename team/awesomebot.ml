cbd23fee9b8f5303198bd4cf9103afe951c9de69
Matt Oey <mso32@cornell.edu>
2012-12-02 02:33:59 -0500

    Update bot
    compiles

--- team/awesomebot.ml
+++ team/awesomebot.ml
@@ -1,6 +1,7 @@
 open Team
 open Definitions
 open Constants
+open Util
 
 (* Attention Student:
  * You only need to modify the handle_request function. Do not change its arguments.
@@ -55,7 +56,7 @@ let handle_request c r =
         
         (* returns if an attack is a special attack or physical attack *)
         let is_special (att: attack) : bool= 
-            match att.steamtype with
+            match att.element with
             | Electric
             | Fire 
             | Water
@@ -89,28 +90,30 @@ let handle_request c r =
             in  
           let power = 
             match is_special att with
-            | true -> (attacker.attack * mod_attack) /. (defender.defense * mod_defense)
-            | false -> starter.spl_attack /. defender.spl_defense  
+            | true -> (float_of_int attacker.attack *. mod_attack) /. (float_of_int defender.defense *. mod_defense)
+            | false -> float_of_int attacker.spl_attack /. float_of_int defender.spl_defense  
             in
           (* let crit = a.crit_chance *. cCRIT_MULTIPLIER /. 100. + (100. - a.crit_chance) /. 100. *)
           let stab = 
             match defender.first_type with
-            | Some t1 ->
+            | Some t1 -> begin
               match defender.second_type with
-              | Some t2 -> if (att.steamtype = t1 || att.steamtype = t2) then cSTAB_BONUS else 1.
-              | None -> if (att.stamtype = t1) then cSTAB_BONUS else 1.
+              | Some t2 -> if (att.element = t1 || att.element = t2) then cSTAB_BONUS else 1.
+              | None -> if (att.element = t1) then cSTAB_BONUS else 1.
+							end
             | None -> 1.   
             in
-          let type_multplier = 
-            match starter.first_type with
-            |  Some t1 ->
-                match starter.second_type with
-                | Some t2 -> (weakness att.steamtype t1) *. (weakness att.steamtype t2)
-                | None -> weakness att.steamtype t1
+          let type_multiplier = 
+            match attacker.first_type with
+            |  Some t1 -> begin
+                match attacker.second_type with
+                | Some t2 -> (weakness att.element t1) *. (weakness att.element t2)
+                | None -> weakness att.element t1
+								end
             | None -> 1.  
             in
           if (att.pp_remaining = 0) then 0.0
-          else att.power *. power *. stab *. type_multiplier /. defender.curr_hp(* *. crit *)
+          else float_of_int att.power *. power *. stab *. type_multiplier /. float_of_int defender.curr_hp
           in
         
         (* Finds if an attack can poison opponent *)
@@ -170,7 +173,7 @@ let handle_request c r =
           
         (* Finds if an attack can increase attacker's speed stat*)
         (* Returns a tuple of the % increase in speed stat and effect chance *)          
-        let speed_up (attacker: steammon) (att: attack): (float * float) option =
+        let speed_up (attacker: steammon) (att: attack): (float * float) =
           let helper1 =
             match attacker.mods.speed_mod with
             | 3 -> 1.
@@ -190,7 +193,7 @@ let handle_request c r =
     
         (* Finds if an attack can increase attacker's defense stat*)
         (* Returns a tuple of the % increase in defense stat and effect chance *)              
-        let defense_up (attacker: steammon) (att: attack): (float * float) option =
+        let defense_up (attacker: steammon) (att: attack): (float * float) =
           let helper1 =
             match attacker.mods.defense_mod with
             | 3 -> 1.
@@ -210,7 +213,7 @@ let handle_request c r =
           
         (* Finds if an attack can increase attacker's accuracy stat*)
         (* Returns a tuple of the % increase in accuracy stat and effect chance *)            
-        let accuracy_up (attacker: steammon) (att: attack) : (float * float) option =
+        let accuracy_up (attacker: steammon) (att: attack) : (float * float) =
           let helper1 =
             match attacker.mods.accuracy_mod with
             | 3 -> 1.
@@ -230,7 +233,7 @@ let handle_request c r =
         
         (* Finds if an attack can decrease defender's attack stat*)
         (* Returns a tuple of the % decrease in attack stat and effect chance *)            
-        let attack_down (defender: steammon) (att: attack) : (float * float) option =
+        let attack_down (defender: steammon) (att: attack) : (float * float) =
           let helper1 =
             match defender.mods.attack_mod with
             | 3 -> cATTACK_UP2 /. cATTACK_UP3 
@@ -250,7 +253,7 @@ let handle_request c r =
           
         (* Finds if an attack can decrease defender's speed stat*)
         (* Returns a tuple of the % decrease in speed stat and effect chance *)            
-        let speed_down (defender: steammon) (att: attack) : (float * float) option =
+        let speed_down (defender: steammon) (att: attack) : (float * float) =
           let helper1 =
             match defender.mods.speed_mod with
             | 3 -> cSPEED_UP2 /. cSPEED_UP3 
@@ -270,7 +273,7 @@ let handle_request c r =
           
         (* Finds if an attack can decrease defender's defense stat*)
         (* Returns a tuple of the % decrease in defense stat and effect chance *)            
-        let defense_down (defender: steammon) (att: attack) : (float * float) option =
+        let defense_down (defender: steammon) (att: attack) : (float * float) =
           let helper1 =
             match defender.mods.defense_mod with
             | 3 -> cDEFENSE_UP2 /. cDEFENSE_UP3 
@@ -290,7 +293,7 @@ let handle_request c r =
           
         (* Finds if an attack can decrease defender's attack stat*)
         (* Returns a tuple of the % decrease in attack stat and effect chance *)            
-        let accuracy_down (defender: steammon) (att: attack) : (float * float) option =
+        let accuracy_down (defender: steammon) (att: attack) : (float * float) =
           let helper1 =
             match defender.mods.accuracy_mod with
             | 3 -> cACCURACY_UP2 /. cACCURACY_UP3 
@@ -333,7 +336,7 @@ let handle_request c r =
           
           let speed_ratio = 
             let mod_speed (s: steammon) : float =
-              match s.mods.mod_speed with
+              match s.mods.speed_mod with
               | 3 -> cSPEED_UP3
               | 2 -> cSPEED_UP2
               | 1 -> cSPEED_UP1
@@ -342,82 +345,84 @@ let handle_request c r =
               | -3 -> cSPEED_DOWN3
               | _ -> 1.
               in
-            let speed = (attacker.speed *. (mod_speed attacker)) /. (defender.speed *. (mod_speed defender)) in
+            let speed = (float_of_int attacker.speed *. (mod_speed attacker)) /. 
+							(float_of_int defender.speed *. (mod_speed defender)) in
             if (List.mem Paralyzed attacker.status) then
               if (is_paralyzed) then speed
-              else speed /. cPARALYSIS_SLOW
+              else speed /. float_of_int cPARALYSIS_SLOW
             else
-              if (is_paralyzed) then 2. * speed
+              if (is_paralyzed) then 2. *. speed
               else speed
             in            
-          let poison_weight (chance: double) : double = 
+          let poison_weight (chance: float) : float = 
             if (has_status) then 0. 
             else (cPOISON_DAMAGE /. (snd (strongest_attack attacker defender))) *. chance
             in
-          let confuse_weight (chance: double) : double =
+          let confuse_weight (chance: float) : float =
             if (is_confused || is_frozen || is_asleep) then 0.
-            else ((100. /. cSNAP_OUT_OF_CONFUSION) - 1.) * 1. /. ((100. /. cSELF_ATTACK_CHANCE) +. 1.) * chance
+            else ((100. /. float_of_int cSNAP_OUT_OF_CONFUSION) -. 1.) /. 
+							((100. /. float_of_int cSELF_ATTACK_CHANCE) +. 1.) *. chance
             in
-          let paralysis_weight (chance: double) : double = 
+          let paralysis_weight (chance: float) : float = 
             if (has_status) then 0.
-            else if ((speed_ratio <= 1.) && (speed_ratio >= (1. /. cPARALYSIS_SLOW))) then chance
-            else (snd (strongest_attack attacker defender)) * 1. /. ((100. /. cPARALYSIS_CHANCE) +. 1.)  * chance
+            else if ((speed_ratio <= 1.) && (speed_ratio >= (1. /. float_of_int cPARALYSIS_SLOW))) then chance
+            else (snd (strongest_attack attacker defender)) *. 1. /. ((100. /. float_of_int cPARALYSIS_CHANCE) +. 1.)  *. chance
             in
-          let sleep_weight (chance: double) : double =
+          let sleep_weight (chance: float) : float =
             if (has_status) then 0.
-            else ((100. /. cWAKE_UP_CHANCE) - 1.) * chance
+            else ((100. /. float_of_int cWAKE_UP_CHANCE) -. 1.) *. chance
             in 
-          let freeze_weight (chance: double) : double =
+          let freeze_weight (chance: float) : float =
             if (has_status) then 0.
-            else ((100. /. cWAKE_UP_CHANCE) - 1.) * chance
+            else ((100. /. float_of_int cWAKE_UP_CHANCE) -. 1.) *. chance
             in
-          let att_up_weight ((increase, chance): double * double) : double =
+          let att_up_weight ((increase, chance): float * float) : float =
             let a = (strongest_attack attacker defender) in
             if (is_special (fst a)) then 0.
             else
-              if (1. /. (snd a)) > ((1. /. ((snd a) *. increase)) + 1.) then chance
+              if (1. /. (snd a)) > ((1. /. ((snd a) *. increase)) +. 1.) then chance
               else 0.
             in
-          let spe_up_weight ((increase, chance): double * double) : double = 
+          let spe_up_weight ((increase, chance): float * float) : float = 
             if ((speed_ratio <= 1.) && (speed_ratio >= (1. /. increase))) then chance else 0.
             in
-          let def_up_weight ((increase, chance): double * double) : double =
+          let def_up_weight ((increase, chance): float * float) : float =
             let a = (strongest_attack defender attacker) in
             if (is_special (fst a)) then 0.
             else
-              if (1. /. (snd a)) < ((increase /. (snd a)) + 1.) then chance
+              if (1. /. (snd a)) < ((increase /. (snd a)) +. 1.) then chance
               else 0.
             in
-          let acc_up_weight ((increase, chance): double * double) : double =
+          let acc_up_weight ((increase, chance): float * float) : float =
             0.
             in
-          let att_down_weight ((decrease, chance): double * double) : double =
+          let att_down_weight ((decrease, chance): float * float) : float =
             let a = (strongest_attack defender attacker) in
             if (is_special (fst a)) then 0.
             else
-              if (1. /. (snd a)) < (1. /. ((snd a) *. increase)) + 1. then chance * 0.5
+              if (1. /. (snd a)) < (1. /. ((snd a) *. decrease)) +. 1. then chance *. 0.5
               else 0.
             in
-          let spe_down_weight ((decrease, chance): double * double) : double =
-            if ((speed_ratio <= 1.) && (speed_ratio >= (decrease))) then chance * 0.5
+          let spe_down_weight ((decrease, chance): float * float) : float =
+            if ((speed_ratio <= 1.) && (speed_ratio >= decrease)) then chance *. 0.5
               else 0.
             in
-          let def_down_weight ((decrease, chance): double * double) : double =
+          let def_down_weight ((decrease, chance): float * float) : float =
             let a = (strongest_attack attacker defender) in
             if (is_special (fst a)) then 0.
             else
-              if (1. /. (snd a)) > (decrease /. (snd a)) + 1. then chance * 0.5
+              if (1. /. (snd a)) > (decrease /. (snd a)) +. 1. then chance *. 0.5
               else 0.
             in
-          let acc_down_weight ((decrease, chance): double * double) : double =
+          let acc_down_weight ((decrease, chance): float * float) : float =
             let a = (strongest_attack defender attacker) in
             if (is_special (fst a)) then 0.
             else
-              if (1. /. (snd a)) > (1. /. ((snd a) *. decrease)) + 1. then chance * 0.5
+              if (1. /. (snd a)) > (1. /. ((snd a) *. decrease)) +. 1. then chance *. 0.5
               else 0.
             in          
-          (attack_power attacker defender att) +. poison_weight (poison_effect att) +. confuse_weight (confuse_effect att) +.
-          paralysis_weight (paralysis_effect att) +. sleep_weight (sleep_effect att) +. freeze_weight (freeze_effect att) +.
+          (attack_power attacker defender att) +. poison_weight (poison_effect att) +. confuse_weight (confused_effect att) +.
+          paralysis_weight (paralyze_effect att) +. sleep_weight (sleep_effect att) +. freeze_weight (freeze_effect att) +.
           sleep_weight (sleep_effect att) +. att_up_weight (attack_up attacker att) +. spe_up_weight (speed_up attacker att) +. 
           def_up_weight (defense_up attacker att) +. acc_up_weight (accuracy_up attacker att) +. 
           att_down_weight (attack_down defender att) +. spe_up_weight (speed_down defender att) +. 
@@ -498,33 +503,35 @@ let handle_request c r =
           else None            
           in
         
-        let item (attacking_team: team_data) (defending_team: team_data) : (item * steammon) option =
+        let items (attacking_team: team_data) (defending_team: team_data) : (item * steammon) option=
           let attacking_pkmn = List.hd (fst attacking_team) in
           let defending_pkmn = List.hd (fst defending_team) in
           let [ethers; max_potions; revives; full_heals; xAttacks; xSpeeds; xDefenses; xAccuracies] = snd attacking_team in
           if (((snd (strongest_attack defending_pkmn attacking_pkmn) >= 1.0) && 
-            ((attacking_pkmn.curr_hp /. attacking_pkmn.max_hp) <= (1. /. 3.))) || 
-            ((attacking_pkmn.curr_hp /. attacking_pkmn.max_hp) <= (1. /. 10.))) &&
+            ((float_of_int attacking_pkmn.curr_hp /. float_of_int attacking_pkmn.max_hp) <= (1. /. 3.))) || 
+            ((float_of_int attacking_pkmn.curr_hp /. float_of_int attacking_pkmn.max_hp) <= (1. /. 10.))) &&
             (max_potions > 0)
-            then Some (MaxPotion * attacking_pkmn)
+            then Some (MaxPotion , attacking_pkmn)
           else if ((List.length (List.filter (fun s -> s.curr_hp = 0) (fst attacking_team))) > 0) && (revives > 0)
-            then Some (Revive * (List.hd (List.filter (fun s -> s.curr_hp = 0) (fst attacking_team))))
+            then Some (Revive , (List.hd (List.filter (fun s -> s.curr_hp = 0) (fst attacking_team))))
           else if ((List.mem Paralyzed attacking_pkmn.status) || (List.mem Poisoned attacking_pkmn.status)) && (full_heals > 0)
-            then Some (FullHeal * attacking_pkmn)
+            then Some (FullHeal , attacking_pkmn)
+					else None
           in
         
         let helper (a: team_data) (b: team_data) : action =
           match switch a b with
           | Some s -> SwitchSteammon s.species
           | None ->
-            match item a b with
+            match items a b with
             | Some (i, s) -> UseItem (i,s.species)
-            | None -> UseAttack (optimal_attack_helper a b)
+            | None -> UseAttack ((optimal_attack_helper a b).name)
           in
-        
-        match c with
+        begin
+        match c with 
         | Red -> helper red_data blue_data
-        | Blue -> helper blue_data red_data
+        | _ -> helper blue_data red_data
+				end
         (*
         (match mons with
         | h::t ->
@@ -545,27 +552,29 @@ let handle_request c r =
         | _ -> failwith "WHAT IN THE NAME OF ZARDOZ HAPPENED HERE")
         *)
         
-    | PickInventoryRequest (gr) -> 
+    | PickInventoryRequest (gs) -> 
       let cash = ref cINITIAL_CASH in
-      let maxPotionCash = cash / 2 in
-      let reviveCash = cash / 3 in
+      let maxPotionCash = !cash / 2 in
+      let reviveCash = !cash / 3 in
       let(r_data, b_data) = gs in
       let (steammon, inventory) = 
         match c with 
         | Red -> r_data
-        | Blue -> b_data in
-            let ethers = 0 in
-            let max_potions = (cash / 2) / cCOST_MAXPOTION in
-            cash := cash - (max_potions * cCOST_MAXPOTION);
-            let revives = (cash / 3) / cCOST_REVIVE in
-            cash := cash - (revives * cCOST_REVIVE);
-            let full_heals = cash / cCOST_FULLHEAL in 
-            cash := cash - (full_heals * cCOST_FULLHEAL);
-            let xattacks = 0 in
-            let xdefenses = 0 in
-            let xaccuracies = 0 in
-            let xspeeds = 0 in
+        | Blue -> b_data 
+				in
+      let ethers = 0 in
+      let max_potions = (!cash / 2) / cCOST_MAXPOTION in
+      cash := !cash - (max_potions * cCOST_MAXPOTION);
+      let revives = (!cash / 3) / cCOST_REVIVE in
+      cash := !cash - (revives * cCOST_REVIVE);
+      let full_heals = !cash / cCOST_FULLHEAL in 
+      cash := !cash - (full_heals * cCOST_FULLHEAL);
+      let xattacks = 0 in
+      let xdefenses = 0 in
+      let xaccuracies = 0 in
+      let xspeeds = 0 in
       PickInventory(
       [ethers; max_potions; revives; full_heals;
       xattacks; xdefenses; xaccuracies; xspeeds])
+			
     let () = run_bot handle_request
